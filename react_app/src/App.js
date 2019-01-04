8a8c089b1877d82a97a5612effa97e8d6f11c618
Andrew Courter and Christy March <acourter@pivotal.io>
2019-01-04 17:08:32 -0600

    Finish POC of scanning and hitting google api for book title and image

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,13 +1,118 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Scanner from './Scanner'
+import Result from './Result'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          Hello World
-      </div>
-    );
-  }
+
+    constructor(props){
+      super(props)
+      this.state = {
+            scanning: false,
+            results: [],
+            bookTitle: null,
+            candidateISBN: null,
+            imageLink: null
+      }
+
+      this._scan = this._scan.bind(this)
+      this._onDetected = this._onDetected.bind(this)
+      this._continueScanning = this._continueScanning.bind(this)
+      this._reachedMaxResults = this._reachedMaxResults.bind(this)
+      this.mode = this.mode.bind(this)
+    }
+
+    // TODO: figure out how to do a secondary sort 
+    mode(arr) {
+      return arr.sort((a,b) =>
+         arr.filter(v => v===a).length - arr.filter(v => v===b).length
+      ).pop()
+    }
+
+    getBookDetails(isbn) {
+      var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn
+
+      fetch(url).then(response => 
+        response.json().then(data => ({
+            data: data,
+            status: response.status
+          }))
+        ).then(res => {
+            if (res.data.totalItems) {
+              var book = res.data.items[0];
+            
+              var title = (book["volumeInfo"]["title"])
+              var subtitle = (book["volumeInfo"]["subtitle"])
+              var authors = (book["volumeInfo"]["authors"])
+              var printType = (book["volumeInfo"]["printType"])
+              var pageCount = (book["volumeInfo"]["pageCount"])
+              var publisher = (book["volumeInfo"]["publisher"])
+              var publishedDate = (book["volumeInfo"]["publishedDate"])
+              var webReaderLink = (book["accessInfo"]["webReaderLink"])
+              var imageLink = (book["volumeInfo"]["imageLinks"]["thumbnail"])
+
+              this.setState({
+                bookTitle: title,
+                imageLink: imageLink
+              })
+            }
+      }).catch(error => {
+          this.setState({
+            bookTitle: 'something went wrong'
+          })
+
+          throw error
+      })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Start'}</button>
+                <ul className="results">
+                    {this.state.results.map((result) => (<Result key={result.codeResult.code} result={result} />))}
+                </ul>
+                <div>{this.state.results.length >= 10 && !this.state.scanning && this.state.candidateISBN}</div>
+                <div>{this.state.results.length >= 10 && !this.state.scanning && this.state.bookTitle}</div>
+                {this.state.results.length >= 10 && !this.state.scanning && <img src={this.state.imageLink} />}
+                {this.state.scanning ? <div><div>Scanning...</div><Scanner onDetected={this._onDetected}/></div> : <div>Not Scanning</div>}
+            </div>
+        );
+    }
+
+    _scan() {
+        this.setState({results: [], scanning: !this.state.scanning})
+    }
+
+    _onDetected(result) {
+        const nextResults = this.state.results.concat([result])
+        this.setState(
+            {
+                results: nextResults,
+                scanning: this._continueScanning(nextResults)
+            }
+        )
+
+        if(this._reachedMaxResults(nextResults)) {
+            const candidateISBN = this.mode(this.state.results.map((result) => result.codeResult.code))
+            this.setState({
+                candidateISBN: candidateISBN
+            })
+            this.getBookDetails(candidateISBN)
+        }
+        return this._reachedMaxResults(nextResults)
+    }
+
+    _continueScanning(nextResults) {
+        if(this._reachedMaxResults(nextResults)) {
+            return false
+        }
+
+        return this.state.scanning
+    }
+
+    _reachedMaxResults(nextResults) {
+        return nextResults.length >= 10
+    }
 }
 
-export default App;
+export default App
---
+++ react_app/src/Scanner.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import Quagga from 'quagga'
+import './Scanner.css'
+
+class Scanner extends Component {
+    constructor(props) {
+      super(props)
+
+      this._onDetected = this._onDetected.bind(this)
+    }
+
+    render() {
+        return (
+            <div id="interactive" className="viewport">
+              <video class="videoCamera" autoplay="true" preload="auto" src="" muted="true" playsinline="true"/>
+              <canvas class="drawingBuffer"/>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        Quagga.init({
+            inputStream: {
+                type : "LiveStream",
+                constraints: {
+                    width: 640,
+                    height: 480,
+                    facing: "environment"
+                },
+                area: { // defines rectangle of the detection/localization area
+                  top: "20%",    // top offset
+                  right: "20%",  // right offset
+                  left: "20%",   // left offset
+                  bottom: "20%"  // bottom offset
+                }
+            },
+            locator: {
+                patchSize: "x-large",
+                halfSample: true
+            },
+            numOfWorkers: 2,
+            decoder: {
+                readers : [ "ean_reader"],
+                    debug: {
+                        showCanvas: true,
+                        showPatches: true,
+                        showFoundPatches: true,
+                        showSkeleton: true,
+                        showLabels: true,
+                        showPatchLabels: true,
+                        showRemainingPatchLabels: true,
+                        boxFromPatches: {
+                            showTransformed: true,
+                            showTransformedBox: true,
+                            showBB: true
+                        }
+                    }
+            },
+            locate: true
+        }, function(err) {
+            if (err) {
+                return console.log(err)
+            }
+            Quagga.start()
+        })
+
+        Quagga.onProcessed(function (result) {
+                var drawingCtx = Quagga.canvas.ctx.overlay
+                var drawingCanvas = Quagga.canvas.dom.overlay
+
+                if (result) {
+                    if (result.boxes) {
+                        drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")))
+                        result.boxes.filter(function (box) {
+                            return box !== result.box
+                        }).forEach(function (box) {
+                            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 })
+                        })
+                    }
+                }
+            })
+
+        Quagga.onDetected(this._onDetected)
+    }
+
+    componentWillUnmount() {
+        Quagga.offDetected(this._onDetected)
+        Quagga.stop()
+    }
+
+    _onDetected(result) {
+        if(this.props.onDetected(result)) {
+          Quagga.stop()  
+        }
+    }
+}
+
+export default Scanner
