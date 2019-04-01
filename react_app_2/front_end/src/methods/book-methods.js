2935e8bce9a58868946d1d2b36f6e0d689a6c725
Adria Navarro <anavarromartin@pivotal.io>
2019-04-01 11:31:36 -0500

    refactor method

--- react_app_2/front_end/src/methods/book-methods.js
+++ react_app_2/front_end/src/methods/book-methods.js
@@ -4,22 +4,27 @@ Array.prototype.flatMap = function(lambda) {
     return Array.prototype.concat.apply([], this.map(lambda));
 };
 
+const booksWithCheckoutHistories = book => book.checkout_histories.length !== 0
+const booksNotReturned = history => !history.checkin_time
+const bookAndHistoryToBookModel = (book, history) => (
+    {
+        id: history.book_id,
+        book_name: book.name,
+        borrower_name: history.name,
+        borrower_email: history.email
+    }
+)
+const bookResponseToBookModels = book => (
+    book.checkout_histories
+        .filter(booksNotReturned)
+        .map(history => bookAndHistoryToBookModel(book, history))
+)
+
 export const getCheckedOutBooks = async (book, doGet = performGet) => (
     (await doGet(`${process.env.REACT_APP_API_URL || window.location.origin}/api/offices/1/books?search=${book}`))
         .data.data.books
-        .filter(book => book.checkout_histories.length !== 0)
-        .flatMap(book =>
-            book.checkout_histories
-                .filter(history => !history.checkin_time)
-                .map(history => (
-                    {
-                        id: history.book_id,
-                        book_name: book.name,
-                        borrower_name: history.name,
-                        borrower_email: history.email
-                    }
-                ))
-        )
+        .filter(booksWithCheckoutHistories)
+        .flatMap(bookResponseToBookModels)
 )
 
 export const returnBook = async (book, doPatch = performPatch) => (
